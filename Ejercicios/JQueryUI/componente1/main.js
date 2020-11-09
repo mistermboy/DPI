2a2c3d5ea71122e1c951005e756700b4cb0c04d0
mistermboy <pabloyo97@hotmail.com>
2020-11-09 09:49:18 +0100

    apuntes

---
+++ Ejercicios/JQueryUI/componente1/main.js
@@ -0,0 +1,71 @@
+let clicks = 0;
+function fillTimeTable(selectedDate) {
+    clicks = 0;
+    let currentDate = new Date(selectedDate)
+    $('#timetable-dates').empty()
+    $('#timetable-times').empty()
+    for (let i = 0; i < 7; i++) {
+        let day = currentDate.getDate()
+        let month = currentDate.getMonth()+1
+        let year = currentDate.getFullYear()
+        let formatedDate = day + "/" + month + "/" + year;
+
+        let dateComponent ='<div class="day">'+formatedDate+'</div>'
+        $('#timetable-dates').append(dateComponent)
+        let morningComponent = '<div id="morning'+i+'" class="morning">Mañana</div>'
+        $('#timetable-times').append(morningComponent)
+        setClickListener( "#morning"+i,"morning")
+        
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+
+    for (let i = 0; i < 7; i++) {
+        let afternoonComponent = '<div id="afternoon'+i+'" class="afternoon">Tarde</div>'
+        $('#timetable-times').append(afternoonComponent)
+        setClickListener( "#afternoon"+i,"afternoon")
+    }
+
+    for (let i = 0; i < 7; i++) {
+        let nightComponent = '<div id="night'+i+'" class="night">Noche</div>'
+        $('#timetable-times').append(nightComponent)
+        setClickListener( "#night"+i,"night")
+    }
+
+
+}
+
+function setClickListener(id,msg){
+    $(id).click(function() {
+        if($(id).css("background-color")=='rgb(200, 230, 201)'){
+            $(id).css("background-color","rgba(0, 0, 0, 0")
+            clicks--
+        }else{
+            if(clicks<3){
+                clicks++
+                $(id).css("background-color","rgb(200, 230, 201)")
+            }
+                
+        }
+       
+    });
+}
+
+$( "#datepicker" ).datepicker();
+
+$("#btn-apply").click(function() {
+    let selectedDate = $( "#datepicker" ).datepicker("getDate");
+    console.log(selectedDate)
+    fillTimeTable(selectedDate);
+    $('#set-time-btn').css('display','inline');
+});
+
+$("#set-time-btn").click(function() {
+    if(clicks>=2)
+        alert('Horario establecido con éxito')
+    else
+        alert('Selecciona al menos 2 franjas')
+});
+
+
+
---
+++ Ejercicios/JQueryUI/componente2/main.js
@@ -0,0 +1,130 @@
+let previousSelected = undefined;
+let currentSelected = undefined;
+let pareja = false;
+let numParejas = 0;
+let numPulsaciones = 0;
+$(function () {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+          effect: "blind",
+          duration: 1000
+        },
+        hide: {
+          effect: "explode",
+          duration: 1000
+        }
+      });
+
+    $("button").button().click(async function (event) {
+        event.preventDefault();
+   
+        if(previousSelected==undefined){
+
+            previousSelected = event.currentTarget.id
+            let img = $('<img>').attr("src", "img/" +extractPngId(previousSelected)+'.png');
+            $(this).append(img)
+           
+        }else{
+
+            currentSelected = event.currentTarget.id
+            let img = $('<img>').attr("src", "img/" +extractPngId(currentSelected)+'.png');
+            $(this).append(img)
+
+            if(extractPngId(previousSelected) == extractPngId(currentSelected)){
+                pareja = true
+           
+                $('#'+previousSelected).unbind()
+                $('#'+previousSelected).click(function(){
+                    window.open(routes[$(this).attr('id').split('-')[0].split('card')[1]-1])
+                })
+
+            
+                $('#'+currentSelected).unbind()
+                $('#'+currentSelected).click(function(){
+                    window.open(routes[$(this).attr('id').split('-')[0].split('card')[1]-1])
+                })
+
+               
+                previousSelected = undefined
+                currentSelected = undefined
+                numParejas++
+            }else{
+                pareja = false
+            }
+        }
+
+        if(!pareja && currentSelected!=undefined){
+            $("#game-container :input").attr("disabled", true);
+            await setTimeout(()=>{
+                $('#'+previousSelected).empty()
+                $('#'+currentSelected).empty()
+                previousSelected = undefined
+                currentSelected = undefined
+                $("#game-container :input").attr("disabled", false);
+            },400)
+            
+        }
+
+        numPulsaciones++
+        $('.num-pulsaciones').text("Pulsaciones: "+numPulsaciones)
+
+        if(numParejas==6){
+            numParejas =0 
+            $( "#dialog" ).dialog( "open" );
+        }
+            
+        
+    });
+});
+
+
+
+
+
+function extractPngId(cardId){
+    if(cardId.includes('p1'))
+        return cardId.split("-p1")[0]
+    return cardId.split("-p2")[0]
+}
+
+function getCards(){
+    let cards = []
+    for(let i=1;i<7;i++){
+        cards.push("card"+i+'-p1.png')
+    }
+    for(let i=1;i<7;i++){
+        cards.push("card"+i+'-p2.png')
+    }
+    return cards
+}
+
+
+function generateCards() {
+    let cards = getCards()
+    let cardNumber = Math.floor(Math.random() * (cards.length - 0))
+    for (let i = 1; i <= 12; i++) {
+        let card = cards.splice(cardNumber, 1)
+        let cardId = card[0].split('.png')[0]
+        let cardBtn = $("<button>")
+        cardBtn.attr("id",cardId)
+        cardBtn.attr("class","card-container")
+        let cardImg = $("<img>")
+        cardImg.attr("src","img/" + card[0].split('-')[0]+".png")
+        $('#game-container').append(cardBtn);
+        cardNumber = Math.floor(Math.random() * (cards.length - 0))
+    }
+
+}
+generateCards()
+
+
+routes = [
+    'https://mtg.gamepedia.com/Black_Lotus',
+    'https://gatherer.wizards.com/pages/card/Details.aspx?name=Deathrite+Shaman',
+    'https://coppermind.net/wiki/Kelsier',
+    'https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=497502',
+    'https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=435220',
+    'https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=435206'
+
+]
